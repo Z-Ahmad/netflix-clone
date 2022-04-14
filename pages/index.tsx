ba49e6bb77f1b2c40a28e476d53700a770dca1e7
Z-Ahmad <zakiahmad42661@gmail.com>
2022-04-14 16:37:56 -0400

    screen gradient and banner component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='relative h-screen bg-gradient-to-b 
+    from-gray-900/10 to-[#010511] lg:h-[140vh]'> 
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,7 +15,7 @@ const Home: NextPage = () => {
       <Header />
       
       <main>
-      {/* Banner */}
+      <Banner/>
       <section>
         {/* row */}
         {/* row */}
